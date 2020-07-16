14d612087ff1458123c922141910b7bb35f26389
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2020-07-16 18:36:05 -0300

    Implentado uso efetivo do dao

--- src/main/java/br/com/livraria/loja/beans/AdminLivrosBean.java
+++ src/main/java/br/com/livraria/loja/beans/AdminLivrosBean.java
@@ -1,8 +1,10 @@
 package br.com.livraria.loja.beans;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import br.com.livraria.loja.daos.LivroDao;
 import br.com.livraria.loja.models.Livro;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,8 +16,12 @@ public class AdminLivrosBean
 {	
 	private Livro livro = new Livro();
 
+	@Inject
+	private LivroDao livroDao;
+	
 	public void salvar()
 	{
+		livroDao.salvar(livro);
 		System.out.println("Livro salvo com sucesso! " + livro);
 	}
 }
--- src/main/java/br/com/livraria/loja/daos/LivroDao.java
+++ src/main/java/br/com/livraria/loja/daos/LivroDao.java
@@ -2,9 +2,11 @@ package br.com.livraria.loja.daos;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 import br.com.livraria.loja.models.Livro;
 
+@Transactional
 public class LivroDao 
 {
 	@PersistenceContext
