38fdd2ead735275bbfc84aebe183789511657517
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2020-07-17 16:33:13 -0300

    Criado modelo de autor e seu campo no formulário principal

--- src/main/java/br/com/livraria/loja/beans/AdminLivrosBean.java
+++ src/main/java/br/com/livraria/loja/beans/AdminLivrosBean.java
@@ -1,10 +1,14 @@
 package br.com.livraria.loja.beans;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.livraria.loja.daos.LivroDao;
+import br.com.livraria.loja.models.Autor;
 import br.com.livraria.loja.models.Livro;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,5 +27,10 @@ public class AdminLivrosBean
 	{
 		livroDao.salvar(livro);
 		System.out.println("Livro salvo com sucesso! " + livro);
+	}
+	
+	public List<Autor> getAutores()
+	{
+		return Arrays.asList(new Autor(1, "Lucas"), new Autor(2, "Luiza"));
 	}
 }
---
+++ src/main/java/br/com/livraria/loja/models/Autor.java
@@ -0,0 +1,13 @@
+package br.com.livraria.loja.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Autor 
+{
+	private Integer id;
+	private String nome;
+}
